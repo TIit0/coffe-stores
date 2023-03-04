b67f17e4825d73f96fd11e0b0b56e92a067199fc
Eduardo O <viverlin7@gmail.com>
2023-03-04 15:51:10 -0600

    getting use location need to update results dynamically

--- hooks/useTrackLocation.js
+++ hooks/useTrackLocation.js
@@ -1,24 +1,43 @@
 
-export default function useTrackLocation() {
+import { useState } from 'react'
 
-    const success = () => {
+const useTrackLocation = () => {
+    const [locationErrorMsg, setLocationErrorMsg] = useState("");
+    const [latLong, setLatLong] = useState("");
+    const [isFindingLocation, setIsFindingLocation] = useState(false)
 
-    }
+    const success = (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        setLatLong(`${latitude},${longitude}`);
+        setLocationErrorMsg("");
+        setIsFindingLocation(false);
+    };
 
     const error = () => {
+        setIsFindingLocation(false);
+        setLocationErrorMsg("Unable to retrieve your location");
+    };
 
-    }
+    const handleTrackLocation = () => {
+        setIsFindingLocation(true);
 
-    function handleTrackLocation() {
         if (!navigator.geolocation) {
-            status.textContent = 'Geolocation is not supported by your browser';
+            setLocationErrorMsg("Geolocation is not supported by your browser");
+            setIsFindingLocation(false);
         } else {
-            status.textContent = 'Locating…';
+            // status.textContent = "Locating…";
             navigator.geolocation.getCurrentPosition(success, error);
         }
-    }
+    };
 
     return {
+        latLong,
+        handleTrackLocation,
+        locationErrorMsg,
+        isFindingLocation
+    };
+};
 
-    }
-}
+export default useTrackLocation;
