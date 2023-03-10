6f4224d030719b18191c4af6f35dd479959f0015
Eduardo O <viverlin7@gmail.com>
2023-03-10 15:16:05 -0600

    before back-end refactor

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -11,49 +11,61 @@ export default async function createCoffeStore(req, res) {
 
     if (req.method === "POST") {
 
-        /* find a store */
-
-        const {id, name, address, locality, voting, imgUrl} = req.body;
+        const { id, name, address, locality, voting, imgUrl } = req.body;
 
         try {
+            if (id) {
 
-            const findCoffeeStoreRecords = await table.select({
-                filterByFormula: `id=${id}`
-            }).firstPage();
+                /* find a store */
 
+                const findCoffeeStoreRecords = await table.select({
+                    filterByFormula: `id=${id}`
+                }).firstPage();
 
-            if (findCoffeeStoreRecords.length !== 0) {
+                if (findCoffeeStoreRecords.length !== 0) {
 
-                const records = findCoffeeStoreRecords.map(record => {
-                    return { ...record.fields }
-                });
+                    const records = findCoffeeStoreRecords.map(record => {
+                        return { ...record.fields }
+                    });
 
-                res.json(records);
+                    res.json(records);
+                } else {
+
+                    /* create/add a store if it contans name & id*/
+
+                    if (name) {
+
+                        const createCoffeeStoreRecord = await table.create([
+                            {
+                                fields: {
+                                    id,
+                                    name,
+                                    address,
+                                    locality,
+                                    voting,
+                                    imgUrl,
+                                },
+                            },
+                        ]);
+
+                        const records = createCoffeeStoreRecord.map(record => {
+                            return { ...record.fields }
+                        });
+
+                        res.json({ records })
+                    } else {
+                        res.status(400);
+                        res.json({ message: "name is missing" });
+                    }
+                }
+
             } else {
-                /* create/add a store */
-
-                const createCoffeeStoreRecord = await table.create([
-                    {
-                        fields: {
-                            id,
-                            name,
-                            address,
-                            locality,
-                            voting,
-                            imgUrl,
-                        },
-                    },
-                ]);
-
-                const records = createCoffeeStoreRecord.map(record => {
-                    return { ...record.fields }
-                });
-
-                res.json({ records })
+                res.status(400);
+                res.json({ message: "Id is missing" });
             }
 
         } catch (err) {
-            console.log(err)
+            console.log("Error creating or finding a store", err)
             res.status(500)
             res.json({ message: `Something went wrong: ${err}` })
         }
