4087ad82ef55c353e37991c01187f9413bfdb34d
Eduardo O <viverlin7@gmail.com>
2023-03-10 14:06:57 -0600

    before creating record

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,19 +1,44 @@
 
 const Airtable = require('airtable');
-const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_KEY);
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_KEY);
 
 
 const table = base('coffe-stores');
 
-console.log({table})
+console.log({ table })
 
-export default function createCoffeStore(req, res) {
-    console.log({req})
-    
-    if (req.method === "POST") {
-        res.json({message: "Hi"})
-    } else {
-        res.json({message: "method is get"});
+export default async function createCoffeStore(req, res) {
+    console.log({ req })
+
+
+    try {
+
+        if (req.method === "POST") {
+
+            /* find a store */
+            const findCoffeeStoreRecords = await table.select({
+                filterByFormula: `id="0"`
+            }).firstPage();
+
+
+            if (findCoffeeStoreRecords.length !== 0) {
+
+                const records = findCoffeeStoreRecords.map(record => {
+                    return { ...record.fields }
+                });
+
+                res.json(records);
+            } else {
+                /* create/add a store */
+                res.json({ message: "create a store" })
+            }
+        }
+
+    } catch (err) {
+        console.log(err)
+        res.status(500)
+        res.json({message: `Something went wrong: ${err}`})
     }
+
 
 }
