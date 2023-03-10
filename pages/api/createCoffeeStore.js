501b8019504933a12bcad7cf33e0b82c6ddb59d1
Eduardo O <viverlin7@gmail.com>
2023-03-10 15:43:57 -0600

    after refactor

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,21 +1,14 @@
-
-const Airtable = require('airtable');
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_KEY);
-
-
-const table = base('coffe-stores');
+import { getMinifiedRecords, table } from "@/lib/airtable";
 
 
 
 export default async function createCoffeStore(req, res) {
-
     if (req.method === "POST") {
 
         const { id, name, address, locality, voting, imgUrl } = req.body;
 
         try {
             if (id) {
-
                 /* find a store */
 
                 const findCoffeeStoreRecords = await table.select({
@@ -24,17 +17,13 @@ export default async function createCoffeStore(req, res) {
 
                 if (findCoffeeStoreRecords.length !== 0) {
 
-                    const records = findCoffeeStoreRecords.map(record => {
-                        return { ...record.fields }
-                    });
+                    const records = getMinifiedRecords(findCoffeeStoreRecords);
 
                     res.json(records);
                 } else {
-
                     /* create/add a store if it contans name & id*/
 
                     if (name) {
-
                         const createCoffeeStoreRecord = await table.create([
                             {
                                 fields: {
@@ -47,10 +36,7 @@ export default async function createCoffeStore(req, res) {
                                 },
                             },
                         ]);
-
-                        const records = createCoffeeStoreRecord.map(record => {
-                            return { ...record.fields }
-                        });
+                        const records = getMinifiedRecords(createCoffeeStoreRecord);
 
                         res.json({ records })
                     } else {
